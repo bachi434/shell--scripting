535dfe35793d47d80d1b635957006a5f69f8d688
basker <bac3433@gmail.com>
2022-10-19 16:20:43 +0530

    added one more line of date

--- 02-vars.sh
+++ 02-vars.sh
@@ -14,3 +14,7 @@ echo today date is $DATE
 EXPR=$((3*7/9+65/85%9))
 echo calculate = $EXPR
 
+read -p "enter your name:" name
+echo "your name $name"
+ read -p "enter your course name:" name
+ echo "your course name $name"
