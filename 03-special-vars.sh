44e083de1128df32cc07d93c798571ac44ac6b1f
basker <bac3433@gmail.com>
2022-10-19 18:22:28 +0530

    special vars

--- 03-special-vars.sh
+++ 03-special-vars.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-echo $0
-echo $1
-echo $2
-echo $3
-echo $#
-echo $*
-echo $@
+echo $0 # list of the name
+echo $1 # 1st arguement
+echo $2 # 2nd arguement
+echo $3 # 3rd arguement
+echo $# # NO of arguement
+echo $* # all arguements
+echo $@ # all argueents
+
 
 
