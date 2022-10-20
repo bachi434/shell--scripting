ab7498ee898a2d48f60083e02ea5d02df89e80da
basker <bac3433@gmail.com>
2022-10-20 15:48:16 +0530

    if condition

--- 04-if conditons.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-### string expressions
-
-a=abc
-if [ $a = "abc" ]; then
-  echo ok
-fi
---
+++ 04-if.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+###string expressions
+a=abc
+if [ $a = abc ]; then
+  echo ok
+fi
