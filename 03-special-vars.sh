1f62e0d29ad7fbea7a3365627d67606e8502a935
basker <bac3433@gmail.com>
2022-10-20 14:51:44 +0530

    roboshop project structure initiation

--- 03-special-vars.sh
+++ 03-special-vars.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-echo $0 # list of the name
-echo $1 # 1st arguement
+echo $0
+  #  list of the name
+echo $1
+# 1st arguement
 echo $2 # 2nd arguement
 echo $3 # 3rd arguement
 echo $# # NO of arguement
 echo $* # all arguements
-echo $@ # all argueents
+echo $@ # all arguements
+
 
 
 
