7e21c6ed8df5f1f564009a0ad3fd1a267e20726c
basker <bac3433@gmail.com>
2022-10-21 21:23:16 +0530

    catalogue installed

--- Roboshop-project/components/catalogue.sh
+++ Roboshop-project/components/catalogue.sh
@@ -74,6 +74,18 @@ cp -r catalogue-main /home/roboshop/catalogue &>>$LOG_FILE
  cd /home/roboshop/catalogue &>>$LOG_FILE
  npm install
 
-chown roboshop:roboshop /home/roboshop/ -R
+chown roboshop:roboshop /home/roboshop/ -R &>>$LOG_FILE
+
+echo "update systemd file"
+sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service &>>$LOG_FILE
+
+echo "setup catalogue systemD file"
+mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>$LOG_FILE
+
+echo "start catalogue"
+systemctl daemon-reload &>>$LOG_FILE
+systemctl enable catalogue &>>$LOG_FILE
+systemctl start catalogue &>>$LOG_FILE
+
 
 
