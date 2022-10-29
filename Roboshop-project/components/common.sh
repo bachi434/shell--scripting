e57e24fd4f64e52da04cda31a118b3b166ea507a
basker <bac3433@gmail.com>
2022-10-29 15:15:51 +0530

    catalogue installed

--- Roboshop-project/components/common.sh
+++ Roboshop-project/components/common.sh
@@ -1,11 +1,13 @@
 LOG_FILE=/tmp/roboshop.log
 rm -f $LOG_FILE
 
+STAT() {
 if [ $1 -eq 0 ]; then
  echo -e "\e[1;31m SUCCESS\e[0m"
   else
  echo -e "\e[1;31m FAILED\e[0m"
-  exit
+  exit 2
 fi
+}
 
 
--- Roboshop-project/roboshop.sh
+++ Roboshop-project/roboshop.sh
@@ -3,8 +3,17 @@
 ID=$(id -u)
 if [ $ID -ne 0 ]; then
   echo -e "\e[1;31m you should be root user to execute to this script..\e[0m"
- exit
+ exit 1
+
  fi
+
+ if [ -f components/$1.sh ]; then
+   bash components/$1.sh
+   else
+     echo -e "\e[1;31mInvalid Input\e[0m"
+     exit 1
+     fi
+
 
 
 
